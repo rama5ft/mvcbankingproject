2b2b70fa584328e4c47b75d5af9dbf9ef7beb473
Muppidi Ramadevi <ramadevi.muppidi2@outlook.com>
2019-10-15 18:00:07 +0530

    bankingproject moviecontroller is created on 15/10/2019 on 17:59

---
+++ mvcbankingproject/Controllers/movieController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcbankingproject.Controllers
+{
+    public class movieController : Controller
+    {
+        // GET: movie
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult details()
+        {
+            return View();
+        }
+    }
+}
